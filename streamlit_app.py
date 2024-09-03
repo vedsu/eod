2cf32ea970773eea8157a4d4cba3d0dce37f7cd9
vedsu <90701239+vedsu@users.noreply.github.com>
2024-09-03 13:24:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import admin.auth as admin_login
 import employee.login as employee_login
 import employee.register as employee_register
+st.set_page_config(page_title="Vybe", page_icon="🚗")
 
 if 'user' not in st.session_state:
     st.session_state.user = None
@@ -14,26 +15,8 @@ if 'default' not in st.session_state:
 
 
 
-if st.session_state.default:
-    employee_login = st.Page("employee/login.py", title = "Member Login", icon=":material/history:",  default=True)
-    employee_register = st.Page("employee/register.py", title = "Member Register", icon=":material/notification_important:")
-    admin_login = st.Page("admin/auth.py", title = "Admin Login", icon = ":material/login:")
-
-    pg = st.navigation(
-        {
-        "Admin":[admin_login],
-        "Team":[employee_login, employee_register]
-    }
-    )
-
-    pg.run()
-else:    
+if st.session_state.default is None :
     
-    # URL of the image
-    image_url = "https://vedsubrandwebsite.s3.amazonaws.com/miscellaneous/letsVybe.webp"
-
-    # Display the image
-    st.image(image_url, caption="Let's Vybe", use_column_width=True)
     st.markdown("""
     <style>
     div.stButton > button {
@@ -46,3 +29,26 @@ else:
     if st.button("click to Vybe!"):
         st.session_state.default = True
         st.rerun()
+    
+    # URL of the image
+    image_url = "https://vedsubrandwebsite.s3.amazonaws.com/miscellaneous/letsVybe.webp"
+
+    # Display the image
+    st.image(image_url, caption="Let's Vybe", use_column_width=True)
+    
+   
+else:    
+    employee_login = st.Page("employee/login.py", title = "Member Login", icon=":material/history:",  default=True)
+    employee_register = st.Page("employee/register.py", title = "Member Register", icon=":material/notification_important:")
+    admin_login = st.Page("admin/auth.py", title = "Admin Login", icon = ":material/login:")
+
+    pg = st.navigation(
+        {
+        "Admin":[admin_login],
+        "Team":[employee_login, employee_register]
+    }
+    )
+
+    pg.run()
+    
+    
