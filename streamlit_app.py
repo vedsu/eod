9573b17cf4cd28cb69c627982d32e7d7ad2064df
vedsu <90701239+vedsu@users.noreply.github.com>
2024-09-23 16:14:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if st.session_state.default is None :
     image_url = "https://vedsubrandwebsite.s3.amazonaws.com/miscellaneous/letsVybe.webp"
     st.markdown(f"""
             <div style='text-align: center;'>
-                <img src="{image_url}" style="border-radius: 50%; width:120px; height:120px; margin-top:10px; box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;">
+                <img src="{image_url}" style="border-radius: 50%; width:200px; height:200px; margin-top:10px; box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;">
             </div>
         """, unsafe_allow_html=True)
     # Display the image
