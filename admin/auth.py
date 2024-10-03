865e913bbd6a05501054285464c5d874cbc386e8
vedsu <90701239+vedsu@users.noreply.github.com>
2024-10-03 13:08:36 +0530

    Update auth.py

--- admin/auth.py
+++ admin/auth.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pymongo
 import time
 import admin.home as home
+import admin.hr as hr
 
 
 if 'user' not in st.session_state:
@@ -31,15 +32,18 @@ collection = db["eod"]
 
 admin_username = st.secrets.admin_username
 admin_passwords = st.secrets.admin_passwords
+hr_username = st.secrets.hr_username
+hr_passwords = st.secrets.hr_passwords
 
 if st.session_state.user and st.session_state.access == 'admin':
     home.main()
-
+elif st.session_state.user and st.session_state.access == "hr":
+    hr.main()
 
 else:
 # def main():
     with st.form(key="auth_form",clear_on_submit=True):
-            st.subheader("Admin Login")
+            st.subheader("Admin/HR Login")
             user = st.text_input("Username :")
             password = st.text_input("Password :", type="password")
             login_button = st.form_submit_button(label = "Login")
@@ -50,5 +54,9 @@ else:
                         st.session_state.user = user
                         st.session_state.access = 'admin'
                         st.rerun()
+                elif user == hr_username and password == hr_passwords:
+                     st.session_state.user = user
+                     st.session_state.access = 'hr'
+                     st.rerun()
                 else:
                     st.error("Invalid credentials! user not authorized try again!")
